9cb1bf6ae15c3bf73f94996d4385c26a9da036bd
ogzCode <oguzguc44@gmail.com>
2023-02-07 11:50:28 +0300

    Style and component codes transferred

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HangManGame from "./components/HangManGame";
+import WordProvider from "./hooks/useWord";
+import { useGameState } from "./hooks/useGameState";
+import GameStart from "./components/GameStart";
 
 function App() {
+  const { state } = useGameState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WordProvider>
+        {
+          state ? <HangManGame /> : <GameStart />
+        }
+      </WordProvider>
     </div>
   );
 }
-
 export default App;
