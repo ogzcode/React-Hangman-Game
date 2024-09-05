c7cb8fb0b758841eb79e96fb3d8341a3caa4602f
ogzCode <oguzguc44@gmail.com>
2024-09-05 11:11:30 +0300

    chore: Add Checkbox component with Tailwind styles

---
+++ src/ui/Checkbox.js
@@ -0,0 +1,43 @@
+import { Check } from "lucide-react-native";
+import * as CheckboxPrimitive from '@rn-primitives/checkbox';
+
+import { useTailwind } from "../context/useTailwind";
+import { useMemo } from "react";
+
+const severityStyle = {
+    info: `bg-blue-500 border-blue-500`,
+    success: `bg-green-500 border-green-500`,
+    warning: `bg-amber-500 border-amber-500`,
+    error: `bg-red-500 border-red-500`,
+    default: `bg-gray-800 border-gray-800`,
+};
+
+export const Checkbox = ({ checked = false, onChange = () => { }, style = {}, disabled = false, severity = "default" }) => {
+    const { tw } = useTailwind();
+
+    const checkboxStyle = useMemo(() => {
+        return [
+            tw.style(`flex-row justify-center items-center w-6 h-6 rounded border disabled:opacity-50`,
+                checked ? `${severityStyle[severity]}` : `bg-white border-gray-400`
+            ),
+            style,
+        ];
+    }, [style, tw, checked, severity]);
+
+    return (
+
+        <CheckboxPrimitive.Root
+            style={checkboxStyle}
+            checked={checked}
+            onCheckedChange={value => onChange(value)}
+            disabled={disabled}
+        >
+            <CheckboxPrimitive.Indicator>
+                {
+                    checked && <Check size={20} style={tw`text-white`} />
+                }
+            </CheckboxPrimitive.Indicator>
+
+        </CheckboxPrimitive.Root>
+    );
+}
